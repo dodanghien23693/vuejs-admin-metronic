ae14739eee68ce50ea025ee6bfb33b433893bae0
Đỗ Đăng Hiển <dodanghien.23693@gmail.com>
2018-07-25 09:16:32 +0700

    Add metronic theme

---
+++ metronic theme/src/js/snippets/apps/mail/mail.js
@@ -0,0 +1,13 @@
+var mMail = function() {
+    
+
+    return {     
+        init: function() {  
+            alert(2);
+        }
+    };
+}();
+
+$(document).ready(function() {
+    mMail.init();
+});
