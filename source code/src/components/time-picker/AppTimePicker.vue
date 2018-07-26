62cca204178f968a5b10a9a8d3ff00e8a9ddab47
Đỗ Đăng Hiển <dodanghien.23693@gmail.com>
2018-07-26 22:10:22 +0700

    Cấu hình tự động format code khi commit code.

--- source code/src/components/time-picker/AppTimePicker.vue
+++ source code/src/components/time-picker/AppTimePicker.vue
@@ -4,20 +4,21 @@
 
 <script>
 export default {
-	name: "app-time-picker",
-	props: ["time"],
-	data() {
-		return {
-			timeok: this.time
-		};
-	},
-	mounted() {
-		$(this.$el).timepicker({
-			defaultTime: "11:45:20 AM",
-			minuteStep: 1,
-			showSeconds: true,
-			showMeridian: true
-		});
-	}
+  name: "app-time-picker",
+  props: ["time"],
+  data() {
+    return {
+      timeok: this.time
+    };
+  },
+  mounted() {
+    $(this.$el).timepicker({
+      defaultTime: "11:45:20 AM",
+      minuteStep: 1,
+      showSeconds: true,
+      showMeridian: true
+    });
+  },
+  beforeDestroy() {}
 };
 </script>
