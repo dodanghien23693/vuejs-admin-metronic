946e3bba03f81db345676484be602d1a28c9945d
Trần Ngọc Minh <ngocminh2306@gmail.com>
2018-07-26 17:31:32 +0700

    slect component

--- source code/src/components/select2/AppSelect2.vue
+++ source code/src/components/select2/AppSelect2.vue
@@ -1,6 +1,45 @@
+<template>
+   <select> 
+    <option v-for="opt in options" :key="opt" :value="opt.value">{{opt.title}}</option>
+  </select>
+</template>
 
 <script>
 export default {
-  name: "app-select2"
+	name: "app-select2",
+	props: ["options", "value", "placeholder", "multiple"],
+	mounted() {
+		var vm = this;
+		$(this.$el)
+			.select2({
+				placeholder: this.placeholder,
+				multiple: this.multiple
+			})
+			.val(this.value)
+			.trigger("change")
+			.on("change", function() {
+				// emit event on change.
+				vm.$emit("input", this.value);
+			});
+	},
+	watch: {
+		value: function(value) {
+			// update value
+			$(this.$el)
+				.val(value)
+				.trigger("change");
+		},
+		options: function(options) {
+			// update options
+			$(this.$el)
+				.empty()
+				.select2({ data: options });
+		}
+	},
+	destroyed: function() {
+		$(this.$el)
+			.off()
+			.select2("destroy");
+	}
 };
 </script>
--- source code/src/pages/user/CreateUserPage.vue
+++ source code/src/pages/user/CreateUserPage.vue
@@ -1,22 +1,39 @@
 <script>
 import template from "./CreateUserPage.html";
+import AppSelect2 from "@/components/select2/AppSelect2.vue";
 export default {
 	template: template,
 	name: "create-user-page",
-	data: {
-		user: {
-			email: "",
-			phone: "",
-			name: "",
-			user_name: "",
-			address: ""
-		}
+	components: {
+		AppSelect2
 	},
+	data() {
+		return {
+			user: {
+				email: "",
+				phone: "",
+				name: "",
+				user_name: "",
+				address: ""
+			},
+			options: [
+				{ title: "option1", value: "1", selected: false },
+				{ title: "option2", value: "2", selected: false },
+				{ title: "option3", value: "3", selected: false }
+			],
+			selected: 2
+		};
+	},
+
 	methods: {
+		addUser() {
+			console.log(this.user);
+		},
 		doLogin() {
 			//gọi request thông qua axios
 			this.$http.post("").then(() => {});
 		}
-	}
+	},
+	mounted() {}
 };
 </script>
