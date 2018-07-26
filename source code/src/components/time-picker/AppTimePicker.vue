2c78590d3bd599fa5acee781c368faeeb3371e41
daohai <daohai1221996@gmail.com>
2018-07-26 17:34:30 +0700

    update component datepicker

--- source code/src/components/time-picker/AppTimePicker.vue
+++ source code/src/components/time-picker/AppTimePicker.vue
@@ -1,6 +1,23 @@
+<template>
+  <input type="text" class="form-control m-input" v-model="timeok" placeholder="Select time">
+</template>
 
 <script>
 export default {
-  name: "app-time-picker"
+	name: "app-time-picker",
+	props: ["time"],
+	data() {
+		return {
+			timeok: this.time
+		};
+	},
+	mounted() {
+		$(this.$el).timepicker({
+			defaultTime: "11:45:20 AM",
+			minuteStep: 1,
+			showSeconds: true,
+			showMeridian: true
+		});
+	}
 };
 </script>
