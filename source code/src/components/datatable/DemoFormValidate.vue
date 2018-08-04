6f72bf55f30e104f0a39ef8041bd8d70bde0e25a
Đỗ Đăng Hiển <dodanghien.23693@gmail.com>
2018-08-04 09:42:15 +0700

    cấu trúc plugins

--- source code/src/components/datatable/DemoFormValidate.vue
+++ source code/src/components/datatable/DemoFormValidate.vue
@@ -11,28 +11,20 @@
 				</div>
 			</div>
 			<!--begin::Form-->
-
 			<form class="m-form m-form--fit m-form--label-align-right" id="m_form_1">
 				<div class="m-portlet__body">
-					
-
-
 					<div class="form-group m-form__group row">
 						<label class="col-form-label col-lg-3 col-sm-12">Email *</label>
 						<div class="col-lg-4 col-md-9 col-sm-12">
 							<input type="text" class="form-control m-input" name="email" placeholder="Enter your email" data-toggle="m-tooltip" title="Tooltip description">
 						</div>
 					</div>
-
 					<div class="form-group m-form__group row">
 						<label class="col-form-label col-lg-3 col-sm-12">phone *</label>
 						<div class="col-lg-4 col-md-9 col-sm-12">
 							<input type="text" class="form-control m-input" name="phone" placeholder="Enter your phone" data-toggle="m-tooltip" title="Tooltip description">
 						</div>
 					</div>
-
-
-
 				</div>
 				<div class="m-portlet__foot m-portlet__foot--fit">
 					<div class="m-form__actions m-form__actions">
@@ -48,7 +40,6 @@
 		</div>
 	</div>
 </template>
-
 <script>
 import $ from "jquery";
 import validate from "jquery-validation";
